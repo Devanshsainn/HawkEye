c5ad99233b5364f274899248ba844d1d387653af
Devansh Sain <210432025+Devanshsainn@users.noreply.github.com>
2026-07-12 02:37:51 +0530

    Configure HawkEye for Render deployment

--- run.py
+++ run.py
@@ -8,8 +8,10 @@ environment = os.getenv("FLASK_ENV", "development")
 app = create_app(environment)
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
-        host="127.0.0.1",
-        port=5000,
+        host="0.0.0.0",
+        port=port,
         debug=app.config["DEBUG"]
     )
